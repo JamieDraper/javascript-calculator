f445f39601aabb30926a3e26d043f481b73ad024
jamiedraper <jamiedrpr@gmail.com>
2016-02-08 02:43:59 +0000

    Handle last char being operator & any division by zero. Add additional styling

--- js/script.js
+++ js/script.js
@@ -1,50 +1,73 @@
 $(document).ready(function(){
 
-
 	$("#display").text("0");
 	var number = "";
 	var operator = "";
     var sumString = "";
+    var wipe = false;
+
+    function lastCharAnOperator() {
+        if (sumString[sumString.length-1] == "*" || sumString[sumString.length-1] == "/" || sumString[sumString.length-1] == "%" || sumString[sumString.length-1] == "+" || sumString[sumString.length-1] == "-") {
+            return true;
+        }
+    }
 
 	// Number listeners
 	$(".numbers").not("#clear, #clearall").click(function() {
-		number += $(this).html();
-		$("#display").text(number);
-
+        if (wipe) {
+            number = "";
+            $("#display").text("0");
+            wipe = false;
+        }
+        if ($(this).html() == "." && number[number.length-1] == ".") {
+            // double decimal, do nothing
+        } else {
+            number += $(this).html();
+            $("#display").text(number);
+        }
 	});
+
 	// Operator listeners
 	$(".operators").click(function() {
+        wipe = false;
 		operator = $(this).text();
         sumString += number;
         number = "";
-        // if last item in item in sumString is an operator, replace with new one
-        if (sumString[sumString.length-1] == "*" || sumString[sumString.length-1] == "/" || sumString[sumString.length-1] == "%" || sumString[sumString.length-1] == "+" || sumString[sumString.length-1] == "-") {
+        // if last item in item in sumString is an operator, replace with new opp
+        if (lastCharAnOperator()) {
             sumString = sumString.slice(0, -1);
             sumString += operator;
         } else {
             sumString += operator;
         }
-
 	});
+
 	// Clear and clearall listeners
 	$("#clear,#clearall").click(function(){
 		number = "";
 		operator = "";
         $("#display").text("0");
 		if ($(this).attr("id") === "clearall") {
-            sumString = "0";
+            sumString = "";
 		}
     });
 
-    // Equal listener
+    // Equals listener
     $("#equals").click(function(){
         sumString += number;
-        // if last item is a char
+        if (lastCharAnOperator()) {
+            sumString = sumString.slice(0, -1);
+        }
     	var answer = eval(sumString);
     	$("#display").text(answer);
+        // handle any division by zero
+        if (answer == "Infinity") {
+            $("#display").text("Error");
+        }
     	operator = "";
         sumString = "";
         number = answer;
+        wipe = true;
     });
 
 });
