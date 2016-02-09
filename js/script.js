7fb617c4d9f9a0bf6d78b4618abe10d2c28b3b8d
jamiedraper <jamiedrpr@gmail.com>
2016-02-09 14:09:38 +0000

    Fix bug allowing for two decimals in number. Make all elements unselectable.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-	$("#display").text("0");
-	var number = "";
-	var operator = "";
+    $("#display").text("0");
+    var number = "";
+    var operator = "";
     var sumString = "";
     var wipe = false;
     var opCount = 0;
@@ -12,27 +12,32 @@ $(document).ready(function(){
             return true;
         }
     }
+  // decimal listener
+    $("#point").click(function(){
+        if (number.indexOf(".") === -1) {
+            // no previous decimals, append
+            number += "."
+            $("#display").text(number);
+        }
+    });
 
-	// Number listeners
-	$(".numbers").not("#clear, #clearall").click(function() {
+    // Number listeners
+    $(".numbers").not("#clear, #clearall, #point").click(function() {
         if (wipe) {
             number = "";
             $("#display").text("0");
             wipe = false;
         }
-        if ($(this).html() == "." && number[number.length-1] == ".") {
-            // double decimal, do nothing
-        } else {
-            number += $(this).html();
-            $("#display").text(number);
-        }
-	});
 
-	// Operator listeners
-	$(".operators").click(function() {
+        number += $(this).html();
+        $("#display").text(number);
+    });
+
+    // Operator listeners
+    $(".operators").click(function() {
         opCount += 1;
         wipe = false;
-		operator = $(this).text();
+        operator = $(this).text();
         sumString += number;
         number = "";
         // intermediate calc
@@ -47,16 +52,16 @@ $(document).ready(function(){
         } else {
             sumString += operator;
         }
-	});
+    });
 
-	// Clear and clearall listeners
-	$("#clear,#clearall").click(function(){
-		number = "";
-		operator = "";
+    // Clear and clearall listeners
+    $("#clear,#clearall").click(function(){
+        number = "";
+        operator = "";
         $("#display").text("0");
-		if ($(this).attr("id") === "clearall") {
+        if ($(this).attr("id") === "clearall") {
             sumString = "";
-		}
+        }
     });
 
     // Equals listener
@@ -66,17 +71,16 @@ $(document).ready(function(){
         if (lastCharAnOperator()) {
             sumString = sumString.slice(0, -1);
         }
-    	var answer = eval(sumString);
-    	$("#display").text(answer);
+        var answer = eval(sumString);
+        $("#display").text(answer);
         // handle any division by zero
         if (answer == "Infinity") {
             $("#display").text("Error");
         }
-    	operator = "";
+        operator = "";
         sumString = "";
         number = answer;
         wipe = true;
     });
 
 });
-
